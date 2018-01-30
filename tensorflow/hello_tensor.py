1f61c758b11bec350c2ccfe5a5696605b2573e50
Derek Chang <chang.weicheng@gmail.com>
2018-01-30 11:22:29 -0500

    no message

---
+++ tensorflow/hello_tensor.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
+
+a = tf.constant(20)
+b = tf.constant(22)
+print('a + b = {0}'.format(sess.run(a+b)))
