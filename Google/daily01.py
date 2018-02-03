335451477706735b728852823e46f956bce13a1b
Derek Chang <chang.weicheng@gmail.com>
2018-02-02 21:27:26 -0500

    no message

---
+++ Google/daily01.py
@@ -0,0 +1,35 @@
+# nodes_1d = [x for x in range(10)]
+# nodes = [[x,y] for x in range(10) for y in range(10)]
+# #node2 = [[x,y] for x, y in range(10) ]
+
+# #print (node2)
+
+
+# for node in nodes:
+# 	print(node)
+
+
+
+
+directions =['R','L','UR','UL','DR','DL']
+node1 = [0,0,[0,1,0,0,0,0]]
+particle = [0, 0, 'direction']
+
+
+node0 = Node()
+
+class Node:
+	def __init__(self):
+		self.x = x
+		self.y = y
+	def node_directionR():
+		return
+
+
+
+
+print(direction1)
+
+print(node1[2])
+print (particle)
+
